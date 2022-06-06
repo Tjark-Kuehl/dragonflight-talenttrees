dfadcd1a88069ce8704a77b3b77b59443313184e
Tjark Kühl <tjark-kuehl@hotmail.com>
2022-06-06 13:00:06 +0200

    fix: husky hooks

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat', // Introducing new features
+      'fix', // Fixing a bug
+      'refactor', // Refactoring code (Not Introducing features or fix)
+      'docs', // add documents
+      'test', // Adding unit tests or e2e test
+      'perf', // Improving performance
+      'revert', // Reverting changes or commits
+      'style', // Updating the UI and style files
+      'build', // build artifacts
+      'ci', // working about CI build system
+      'wip', // Work in progress
+      'chore', // Work with configuration or other stuff
+    ]],
+  },
+};
