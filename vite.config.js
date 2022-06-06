32f0f6f3e2958910308c88480be8ef63e4c39322
Tjark Kühl <tjark-kuehl@hotmail.com>
2022-06-06 13:24:28 +0200

    wip: try to fix vite env

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
-export default defineConfig({
-  base: `${import.meta.env.BASE_URL || ''}/`,
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const environment = loadEnv(mode, process.cwd(), '');
+  return {
+    base: `${environment.BASE_URL || ''}/`,
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('src', import.meta.url)),
+      },
     },
-  },
+    define: {
+      __APP_ENV__: environment.APP_ENV,
+    },
+  };
 });
