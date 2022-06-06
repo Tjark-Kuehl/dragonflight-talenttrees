6eee63fbc8ce1a2afb2c8169b3224e9d793986c0
Tjark Kühl <tjark-kuehl@hotmail.com>
2022-06-06 14:26:50 +0200

    fix(build): path rewrite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,15 @@
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
-export default defineConfig(({ mode }) => {
-  const environment = loadEnv(mode, process.cwd(), '');
-  return {
-    base: `${environment.BASE_URL || ''}/`,
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('src', import.meta.url)),
-      },
+export default defineConfig({
+  base: `${process.env.NODE_ENV === 'production' ? '/dragonflight-talenttrees' : ''}/`,
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('src', import.meta.url)),
     },
-    define: {
-      __APP_ENV__: environment.APP_ENV,
-    },
-  };
+  },
 });
